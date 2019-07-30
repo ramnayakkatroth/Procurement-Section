bd01b86b9948d31c376dd4ef3383732bd7a971cd
Katroth Ram <katrothramnayak@gmail.com>
2019-07-31 00:44:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
 const {createServer} = require('http')
 app.prepare().then(() => {
-  createServer(handler).listen(3129,(err)=>{
+  createServer(handler).listen(80,(err)=>{
     if(err) throw err;
     else
       console.log('Ready on localhost:80');
